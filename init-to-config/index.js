6e6152e5f5d395230d68eed50f040348d58c046c
m7he4rt <mateuspm2015@hotmail.com>
2020-09-29 00:33:19 -0300

    Primeiro setup do 0 em reason

---
+++ init-to-config/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './src/App.bs';
+
+ReactDOM.render(
+  <App name="Mateus" />,
+  document.getElementById("root"),
+);
---
+++ init-to-config/src/Index.re
@@ -0,0 +1,4 @@
+ReactDOMRe.renderToElementWithId(
+  <App name="Mateus" />,
+  "root"
+)
